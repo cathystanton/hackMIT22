0171cb1798349526ade9214ccb6d96147af959ae
wilkyrlx <john_wilkinson@brown.edu>
2022-10-01 14:02:59 -0400

    spotify api in backend - reference only

--- backend/songJournal.py
+++ backend/songJournal.py
@@ -1,21 +1,38 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
-
+import sys
 import os
 
-# setup for Spotify Web API
-# hardcoded for demo
-os.environ['SPOTIPY_CLIENT_ID'] = '705f9d56de324473972b2ac2c7368a93'
-os.environ['SPOTIPY_CLIENT_SECRET'] = 'f502cba5baf442dbbc662dc778be76dc'
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+scope = 'user-library-read'
+
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    print("Usage: %s username" % (sys.argv[0],))
+    sys.exit()
+
+token = util.prompt_for_user_token(username,
+                           scope,
+                           client_id='705f9d56de324473972b2ac2c7368a93',
+                           client_secret='f502cba5baf442dbbc662dc778be76dc',
+                           redirect_uri='http://localhost/8080')
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    results = sp.current_user_saved_tracks()
+    for item in results['items']:
+        track = item['track']
+        print(track['name'] + ' - ' + track['artists'][0]['name'])
+else:
+    print("Can't get token for", username)
 
 
 birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
-results = spotify.current_user_recently_played(10)
+results = sp.current_user_recently_played(10)
 albums = results['items']
 while results['next']:
-    results = spotify.next(results)
+    results = sp.next(results)
     albums.extend(results['items'])
 
 for album in albums:
